f20b6ee94c2f30759b4cb713aeffc1b16c2bdb66
Ryan Chui <ryan.w.chui@gmail.com>
2017-08-21 18:56:50 -0700

    psuhing to remote

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './components/App';
+import registerServiceWorker from './components/registerServiceWorker';
+import './stylesheets/index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
